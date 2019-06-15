c4256beb3074ea709d1c027502634556dff42a80
Luciano Baschiera <lucianobaschiera@gmail.com>
2019-06-15 16:50:29 -0300

    Implementación de 2 a) con arcos ordenados con y sin heuristicas. Limpieza de warnings en DLL

--- src/Kruskal.java
+++ src/Kruskal.java
@@ -1,8 +1,10 @@
 import TDAColaPrioridad.Heap;
 import TDAColaPrioridad.Pesado;
+import TDADisjointSet.DisjointSet;
+import TDADisjointSet.DisjointSetSH;
+import TDADisjointSet.NodoDisjoint;
+import TDADisjointSet.NodoDisjointSH;
 import TDALista.DoubleLinkedList;
-
-import java.util.Iterator;
 
 public class Kruskal {
     private Grafo graph;
@@ -12,18 +14,11 @@ public class Kruskal {
     }
 
     public DoubleLinkedList<Pesado> arcosOrdenadosCH() throws Exception {
-        DoubleLinkedList<Pesado> ar=new DoubleLinkedList<>();
+        DoubleLinkedList<Pesado> ar=ordernarLista();
         DoubleLinkedList<Pesado> t=new DoubleLinkedList<>();
-        Heap heap=new Heap(graph.getArcosCount());
-        for (Iterator<Pesado> arco = graph.getArcos().iterator(); arco.hasNext();) {
-            heap.insert(arco.next());
-        }
-        while (!heap.isEmpty()){
-            ar.addLast(heap.removeMin());
-        }
         int n=graph.getNodosCount();
         DisjointSet d =new DisjointSet(graph.getNodosCount());
-        d.initialize(graph.getNodos());
+        d.initiate(graph.getNodos());
         do{
             Pesado e=ar.removeFirst();
             NodoDisjoint compu= d.find(e.getArco().getNodo1());
@@ -37,27 +32,32 @@ public class Kruskal {
     }
 
     public DoubleLinkedList<Pesado> arcosOrdenadosSH() throws Exception {
-        DoubleLinkedList<Pesado> ar=new DoubleLinkedList<>();
+        DoubleLinkedList<Pesado> ar=ordernarLista();
         DoubleLinkedList<Pesado> t=new DoubleLinkedList<>();
-        Heap heap=new Heap(graph.getArcosCount());
-        for (Iterator<Pesado> arco = graph.getArcos().iterator(); arco.hasNext();) {
-            heap.insert(arco.next());
-        }
-        while (!heap.isEmpty()){
-            ar.addLast(heap.removeMin());
-        }
         int n=graph.getNodosCount();
         DisjointSetSH d =new DisjointSetSH(graph.getNodosCount());
-        d.initialize(graph.getNodos());
+        d.initiate(graph.getNodos());
         do{
             Pesado e=ar.removeFirst();
-            NodoDisjoint compu= d.find(e.getArco().getNodo1());
-            NodoDisjoint compv= d.find(e.getArco().getNodo2());
+            NodoDisjointSH compu= d.find(e.getArco().getNodo1());
+            NodoDisjointSH compv= d.find(e.getArco().getNodo2());
             if (compu!=compv){
                 d.union(compu.getElemento(), compv.getElemento());
                 t.addLast(e);
             }
         } while (t.size()< n-1);
         return t;
+    }
+
+    private DoubleLinkedList<Pesado> ordernarLista() throws Exception {
+        DoubleLinkedList<Pesado> ar=new DoubleLinkedList<>();
+        Heap heap=new Heap(graph.getArcosCount());
+        for (Pesado pesado : graph.getArcos()) {
+            heap.insert(pesado);
+        }
+        while (!heap.isEmpty()){
+            ar.addLast(heap.removeMin());
+        }
+        return ar;
     }
 }
--- src/TDADisjointSet/DisjointSetSH.java
+++ src/TDADisjointSet/DisjointSetSH.java
@@ -6,7 +6,7 @@ import TDALista.NodoD;
 
 import java.util.ArrayList;
 
-class DisjointSetSH {
+public class DisjointSetSH {
 
     DoubleLinkedList<NodoDisjointSH> parentList;
     NodoD<NodoDisjointSH>[] nodos;
@@ -26,13 +26,13 @@ class DisjointSetSH {
     }
 
     // Creates n sets with single item in each
-    void makeSet(int n) {
+    public void makeSet(int n) {
         NodoDisjointSH nodo = new NodoDisjointSH(n,null,0);
         nodos[n-1] = parentList.addFirst(nodo);
     }
 
     // Returns representative of x's set
-    NodoDisjointSH find(int x) {
+    public NodoDisjointSH find(int x) {
 
         if (nodos[x-1].element().getPadre() != null) {
             // if x is not the parent of itself
@@ -49,7 +49,7 @@ class DisjointSetSH {
 
     // Unites the set that includes x and the set
     // that includes x
-    void union(int x, int y) throws InvalidPositionException {
+    public void union(int x, int y) throws InvalidPositionException {
         // Find representatives of two sets
         NodoDisjointSH xRoot = find(x), yRoot = find(y);
 
