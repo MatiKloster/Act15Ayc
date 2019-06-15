6d04c8bead4c86b7e57c0f3c6d18d9eab2d35b65
Luciano Baschiera <lucianobaschiera@gmail.com>
2019-06-15 17:21:33 -0300

    Conectitud con disjoint set

--- src/Conexo.java
+++ src/Conexo.java
@@ -1,4 +1,20 @@
+import TDAColaPrioridad.Pesado;
+import TDADisjointSet.DisjointSet;
+import TDADisjointSet.NodoDisjoint;
+
 public class Conexo {
-
-
+    public boolean conexoDS(Grafo graph){
+        DisjointSet d= new DisjointSet(graph.getNodosCount());
+        d.initiate(graph.getNodos());
+        for (Pesado pesado: graph.getArcos()){
+            NodoDisjoint compu = d.find(pesado.getArco().getNodo1());
+            NodoDisjoint compv = d.find(pesado.getArco().getNodo2());
+            if (compu!=compv){
+                d.union(compu.getElemento(), compv.getElemento());
+            }
+            if (d.parentSize()==1)
+                break;
+        }
+        return d.parentSize()==1;
+    }
 }
