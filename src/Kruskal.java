07007a8785dc11865c67345485a5525261bac6ca
Luciano Baschiera <lucianobaschiera@gmail.com>
2019-06-15 16:31:44 -0300

    asda

--- src/Kruskal.java
+++ src/Kruskal.java
@@ -1,5 +1,6 @@
 import TDAColaPrioridad.Heap;
 import TDAColaPrioridad.Pesado;
+import TDALista.DoubleLinkedList;
 
 import java.util.Iterator;
 
@@ -10,14 +11,53 @@ public class Kruskal {
         this.graph=graph;
     }
 
-    public Pesado[] arcosOrdenados(){
-        Pesado[] ar=new Pesado[graph.getArcosCount()];
+    public DoubleLinkedList<Pesado> arcosOrdenadosCH() throws Exception {
+        DoubleLinkedList<Pesado> ar=new DoubleLinkedList<>();
+        DoubleLinkedList<Pesado> t=new DoubleLinkedList<>();
         Heap heap=new Heap(graph.getArcosCount());
         for (Iterator<Pesado> arco = graph.getArcos().iterator(); arco.hasNext();) {
             heap.insert(arco.next());
         }
         while (!heap.isEmpty()){
-            
+            ar.addLast(heap.removeMin());
         }
+        int n=graph.getNodosCount();
+        DisjointSet d =new DisjointSet(graph.getNodosCount());
+        d.initialize(graph.getNodos());
+        do{
+            Pesado e=ar.removeFirst();
+            NodoDisjoint compu= d.find(e.getArco().getNodo1());
+            NodoDisjoint compv= d.find(e.getArco().getNodo2());
+            if (compu!=compv){
+                d.union(compu.getElemento(), compv.getElemento());
+                t.addLast(e);
+            }
+        } while (t.size()< n-1);
+        return t;
+    }
+
+    public DoubleLinkedList<Pesado> arcosOrdenadosSH() throws Exception {
+        DoubleLinkedList<Pesado> ar=new DoubleLinkedList<>();
+        DoubleLinkedList<Pesado> t=new DoubleLinkedList<>();
+        Heap heap=new Heap(graph.getArcosCount());
+        for (Iterator<Pesado> arco = graph.getArcos().iterator(); arco.hasNext();) {
+            heap.insert(arco.next());
+        }
+        while (!heap.isEmpty()){
+            ar.addLast(heap.removeMin());
+        }
+        int n=graph.getNodosCount();
+        DisjointSetSH d =new DisjointSetSH(graph.getNodosCount());
+        d.initialize(graph.getNodos());
+        do{
+            Pesado e=ar.removeFirst();
+            NodoDisjoint compu= d.find(e.getArco().getNodo1());
+            NodoDisjoint compv= d.find(e.getArco().getNodo2());
+            if (compu!=compv){
+                d.union(compu.getElemento(), compv.getElemento());
+                t.addLast(e);
+            }
+        } while (t.size()< n-1);
+        return t;
     }
 }
