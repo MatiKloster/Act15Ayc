09e722c62671b7118208a65e75db4d5f53bcb64e
Luciano Baschiera <lucianobaschiera@gmail.com>
2019-06-14 21:18:59 -0300

    extracción de heapify a un método propio

--- src/TDAColaPrioridad/Heap.java
+++ src/TDAColaPrioridad/Heap.java
@@ -3,10 +3,9 @@ package TDAColaPrioridad;
 public class Heap{
 	private Pesado [] ar;
 	private int size;
-	private int maxElements=100;
 	
-	public Heap(){
-		this.ar=new Pesado[maxElements];
+	public Heap(int cant){
+		this.ar=new Pesado[cant];
 		this.size=0;
 	}
 	
@@ -25,66 +24,61 @@ public class Heap{
 			return ar[1];
 	}
 
-	
 	public void insert(Pesado p) {
-		if (size == maxElements - 1) {
-			Pesado[] aux = new Pesado[maxElements * 2];
-			for (int i = 1; i < maxElements; i++) {
-				aux[i] = ar[i];
-				this.ar = aux;
-				maxElements *= 2;
-			}
-			ar[++size] = p;
-			int i = size;
-			boolean sigo = true;
-			while (i > 1 && sigo) {
-				Pesado elemActual = ar[i];
-				Pesado elemPadre = ar[(i / 2)];
-				if (elemActual.getPeso() < elemPadre.getPeso()) {
-					ar[i] = ar[i / 2];
-					ar[i / 2] = elemActual;
-					i /= 2;
-				} else {
-					sigo = false;
-				}
+		ar[++size] = p;
+		int i = size;
+		boolean sigo = true;
+		while (i > 1 && sigo) {
+			Pesado elemActual = ar[i];
+			Pesado elemPadre = ar[(i / 2)];
+			if (elemActual.getPeso() < elemPadre.getPeso()) {
+				ar[i] = ar[i / 2];
+				ar[i / 2] = elemActual;
+				i /= 2;
+			} else {
+				sigo = false;
 			}
 		}
 	}
 
 	public Pesado removeMin() throws Exception {
-		Pesado ent=min();
+		Pesado arco=min();
 		if(size==1){
 			ar[size--]=null;
-			return ent;
+			return arco;
 		}
 		else{
 			ar[1]=ar[size]; ar[size--]=null;
 			int i=1;
-			boolean seguir=true;
-			while(seguir){
-				int hDer=(i*2)+1,hIzq=i*2;
-				int m=0;
-				boolean tieneHIzq=hIzq<=size,tieneHDer=hDer<=size;
-				if(!tieneHIzq) seguir=false;
-				else{
-					if(tieneHDer){
-						if(ar[hIzq].getPeso() < ar[hDer].getPeso())
-							m=hIzq;
-						else
-							m=hDer;
-					}
-					else m=hIzq;
-				}
-			
-				if(seguir && ar[hIzq].getPeso() > ar[hDer].getPeso()){
-					Pesado aux=ar[i];
-					ar[i]=ar[m];
-					ar[m]=aux;
-					i=m;
-				}
-				else seguir=false;
-			}
+			heapify(i);
 		}
-		return ent;
+		return arco;
+	}
+
+	public void heapify(int i){
+		boolean seguir=true;
+		while(seguir){
+			int hDer=(i*2)+1,hIzq=i*2;
+			int m=0;
+			boolean tieneHIzq=hIzq<=size,tieneHDer=hDer<=size;
+			if(!tieneHIzq) seguir=false;
+			else{
+				if(tieneHDer){
+					if(ar[hIzq].getPeso() < ar[hDer].getPeso())
+						m=hIzq;
+					else
+						m=hDer;
+				}
+				else m=hIzq;
+			}
+
+			if(seguir && ar[hIzq].getPeso() > ar[hDer].getPeso()){
+				Pesado aux=ar[i];
+				ar[i]=ar[m];
+				ar[m]=aux;
+				i=m;
+			}
+			else seguir=false;
+		}
 	}
 }
