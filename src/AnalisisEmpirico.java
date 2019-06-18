5fa333ce7b79db797ddedccf5765ac88bbea6d09
matiKloster <matikloster2@gmail.com>
2019-06-18 20:13:32 -0300

    readytoflip

--- src/AnalisisEmpirico.java
+++ src/AnalisisEmpirico.java
@@ -14,61 +14,59 @@ public class AnalisisEmpirico{
 
     public static void main(String[] args) throws Exception {
         Grafo grafo;
-        DoubleLinkedList<ResultadoConexo> lista=new DoubleLinkedList<>();
+        DoubleLinkedList<Resultado> lista=new DoubleLinkedList<>();
         grafo = getGrafo(5,4,0);
-        lista.addFirst(analisisConectitudBFS(grafo));
         lista.addFirst(analisisConectitudDS(grafo));
+        lista.addFirst(analisisConectitudBFS(grafo));
         TimeUnit.MILLISECONDS.sleep(500);
         grafo = getGrafo(50,49,0);
-        lista.addFirst(analisisConectitudBFS(grafo));
         lista.addFirst(analisisConectitudDS(grafo));
+        lista.addFirst(analisisConectitudBFS(grafo));
         TimeUnit.MILLISECONDS.sleep(500);
         grafo = getGrafo(150,150,0);
-        lista.addFirst(analisisConectitudBFS(grafo));
         lista.addFirst(analisisConectitudDS(grafo));
+        lista.addFirst(analisisConectitudBFS(grafo));
         TimeUnit.MILLISECONDS.sleep(500);
-        grafo = getGrafo(333, 332,0);
-        lista.addFirst(analisisConectitudBFS(grafo));
+        grafo = getGrafo(333, 54236,0);
         lista.addFirst(analisisConectitudDS(grafo));
+        lista.addFirst(analisisConectitudBFS(grafo));
         TimeUnit.MILLISECONDS.sleep(500);
-        grafo = getGrafo(456, 500,0);
-        lista.addFirst(analisisConectitudBFS(grafo));
+        grafo = getGrafo(456, 100000,0);
         lista.addFirst(analisisConectitudDS(grafo));
+        lista.addFirst(analisisConectitudBFS(grafo));
         TimeUnit.MILLISECONDS.sleep(500);
         grafo = getGrafo(500, 124750,0);
-        lista.addFirst(analisisConectitudBFS(grafo));
         lista.addFirst(analisisConectitudDS(grafo));
+        lista.addFirst(analisisConectitudBFS(grafo));
         TimeUnit.MILLISECONDS.sleep(500);
         int i=1;
-        for(ResultadoConexo rs: lista){
+        for(Resultado rs: lista){
             System.out.println("Grafo numero:"+i+++", Conexo: "+rs.isResultado()+" time:"+rs.getTime()+"ns");
         }
     }
-    private static ResultadoConexo analisisConectitudDS(Grafo grafo) throws InvalidPositionException {
+    private static Resultado analisisConectitudDS(Grafo grafo) throws InvalidPositionException {
         Conexo conexoHelper = new Conexo();
         boolean resultado;
         long startTime = System.nanoTime();
         resultado = conexoHelper.conexoDS(grafo);
         long endtime = System.nanoTime();
-        return new ResultadoConexo(resultado, endtime - startTime );
+        return new Resultado(resultado, endtime - startTime );
     }
-    private static ResultadoConexo analisisConectitudBFS(Grafo grafo) throws EmptyQueueException {
+    private static Resultado analisisConectitudBFS(Grafo grafo) throws EmptyQueueException {
         Conexo conexoHelper =new Conexo();
         boolean resultado;
         long  startTime = System.nanoTime();
         resultado=conexoHelper.conexoBFS(grafo);
         long endtime=System.nanoTime();
-        return new ResultadoConexo(resultado,endtime-startTime);
+        return new Resultado(resultado,endtime-startTime);
     }
 
     private static void analisisArbolMinimoCubrimiento(Grafo grafo) throws Exception{
         Kruskal kruskal =new Kruskal(grafo);
-        System.out.println("/////////Kruskal con Heap (sin heurísiticas)/////////");
-        System.out.println("Se prodecera a obtener el arbol mínimo de cubrimiento con heap y controlando su tiempo:");
-        Instant start= Instant.now();
-        DoubleLinkedList<Pesado> resultado=kruskal.conHeapSH();
-        Instant finish=Instant.now();
-        long timeElapsed= Duration.between(start, finish).getNano();
+        DoubleLinkedList<Pesado> resultado;
+        long  startTime = System.nanoTime();
+        resultado=kruskal.conHeapSH();
+        long endtime=System.nanoTime();
         System.out.println("Grafo con "+ grafo.getNodosCount() + " nodos y "+ grafo.getArcosCount() + " arcos resultados con Kruskal con heap:");
         System.out.print("Arbol: {");
         for (Pesado arco : resultado){
